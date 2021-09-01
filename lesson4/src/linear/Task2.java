1b04b308b8a8acd9183d949ec5528eb37c6f2611
Evgen <Evgen@localhost>
2021-09-01 14:54:17 +0300

    Add validation, info for users. Logic edit.

---
+++ lesson4/src/linear/Task2.java
@@ -0,0 +1,23 @@
+package linear;
+
+import java.util.Scanner;
+
+public class Task2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter double number");
+		double num1;
+		for (int i = 0; i < 3; i++) {
+			if (sc.hasNextDouble()) {
+				num1 = sc.nextDouble();
+				double expr1 = (num1 * 1000) % 1000;
+				double expr2 = (int) num1 / 1000.0;
+				double result = expr1 + expr2;
+				System.out.println(result);
+			} else {
+				System.out.println("Incorrect data! Please re-enter!");
+				sc.next();
+			}
+		}
+	}
+}
