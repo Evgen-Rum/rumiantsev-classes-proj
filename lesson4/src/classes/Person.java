50765ee17d86964145c9a86eddcc43b2520c93e4
Evgen <Evgen@localhost>
2021-09-02 16:47:49 +0300

    Create Person and parameters

---
+++ lesson4/src/classes/Person.java
@@ -0,0 +1,24 @@
+package classes;
+
+public class Person {
+	private String firstName;
+	private String lastName;
+	private int age;
+	private long id;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public long getId() {
+		return id;
+	}
+}
