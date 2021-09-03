880d12afb60c8aab14cf739acee1c524397a190f
Evgen <Evgen@localhost>
2021-09-03 13:44:16 +0300

    add some metods

--- lesson4/src/classes/AccountantUtil.java
+++ lesson4/src/classes/AccountantUtil.java
@@ -1,6 +1,36 @@
 package classes;
 
 public class AccountantUtil extends Employee {
+
+	public boolean addSalary(Employee empl) {
+		if (getPosition().equals("Cleaner")) {
+			if (getExperience() > 0 && getExperience() < 5) {
+				empl.setSalary(500);
+				return true;
+			} else if (getExperience() > 5 && getExperience() < 10) {
+				empl.setSalary(800);
+				return true;
+			} else if (getExperience() > 10) {
+				empl.setSalary(1100);
+				return true;
+			} else {
+				return false;
+			}
+		} else if (getPosition().equals("Teacher")) {
+			if (getExperience() > 0 && getExperience() < 5) {
+				empl.setSalary(1000);
+				return true;
+			} else if (getExperience() > 5 && getExperience() < 10) {
+				empl.setSalary(1500);
+				return true;
+			} else if (getExperience() > 10) {
+				empl.setSalary(2000);
+				return true;
+			}
+		}
+		return false;
+	}
+
 	public boolean addPrize(Employee empl) {
 		if (getExperience() > 0 && getExperience() < 15) {
 			empl.setPrize(getSalary() / 3);
