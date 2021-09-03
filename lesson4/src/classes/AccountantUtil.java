781ae50fb6857506d5240ec1e46d82f32a5359ee
Evgen <Evgen@localhost>
2021-09-03 13:25:44 +0300

    create two metods

--- lesson4/src/classes/AccountantUtil.java
+++ lesson4/src/classes/AccountantUtil.java
@@ -1,5 +1,21 @@
 package classes;
 
 public class AccountantUtil extends Employee {
-	
+	public boolean addPrize(Employee empl) {
+		if (getExperience() > 0 && getExperience() < 15) {
+			empl.setPrize(getSalary() / 3);
+			return true;
+		} else if (getExperience() > 15) {
+			empl.setPrize(getSalary() / 2);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean addHolidayPay(Employee empl) {
+		empl.setHolidayPay(getSalary() * 2.5);
+		return true;
+	}
+
 }
