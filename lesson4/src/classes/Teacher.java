0aec0362a71507ac15a97f6148ff0004ed701bcd
Evgen <Evgen@localhost>
2021-09-08 22:30:44 +0300

    fix method toString

--- lesson4/src/classes/Teacher.java
+++ lesson4/src/classes/Teacher.java
@@ -2,13 +2,9 @@ package classes;
 
 public class Teacher extends Employee {
 
-
 	public String toString() {
-		return "[Employee info: Name - " + getFirstName() + " " + getLastName() + "; ID - " + getId() + "; Age - "
-				+ getAge() + "; Academic degree - " + getAcademDegree() + "; Position - " + getPosition()
-				+ "; Salary - " + getSalary() + " BYN" +  "; Experience - " + getExperience() + "]";
+		return "[Employee info:  ID - " + getId() + "; Academic degree - " + getAcademDegree() + "; Position - "
+				+ getPosition() + "; Salary - " + getSalary() + " BYN" + "; Experience - " + getExperience() + "]";
 	}
-
-
 
 }
