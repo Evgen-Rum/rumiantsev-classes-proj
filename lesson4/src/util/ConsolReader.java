4226b86672336cd660108da9a0683f1a9999b3bf
Evgenii Rumiantsev <new.twunk@mail.ru>
2021-09-11 16:16:28 +0300

    Add method "readIntFromConsole"

--- lesson4/src/util/ConsolReader.java
+++ lesson4/src/util/ConsolReader.java
@@ -1,5 +1,25 @@
 package util;
 
-public class ConsolReader {
+import java.util.Scanner;
 
+public class ConsolReader {
+	public static int readIntFromConsole(int defaultValue) {
+		System.out.println("Please enter whole number!");
+		Scanner scan = new Scanner(System.in);
+		int num;
+		for (int i = 0; i < 3; i++) {
+			if (scan.hasNextInt()) {
+				num = scan.nextInt();
+				return num;
+			} else if (i < 2) {
+				System.out.println("Incorrect value! Please re-enter!");
+				scan.next();
+			} else {
+				num = defaultValue;
+				return num;
+			}
+		}
+		return defaultValue;
+
+	}
 }
