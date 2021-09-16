c007f5dd79e8d28a0a03a34ce7ca54c87107e3cd
Evgenii Rumiantsev <new.twunk@mail.ru>
2021-09-16 15:09:26 +0300

    change logic for method addStudents

--- lesson4/src/classes/Group.java
+++ lesson4/src/classes/Group.java
@@ -1,44 +1,48 @@
 package classes;
 
+import datastructures.DynArrImpl;
+import datastructures.MyCollection;
+
 public class Group implements Identifiable {
 	private String groupName;
 	private Teacher curator;
 	private String specializ;
 	private int numberOfStudents;
 	private long id;
-	private Student[] students = new Student[18];
+	private MyCollection students = new DynArrImpl();
 
 	public Group() {
 		id = (long) (Math.random() * Long.MAX_VALUE);
 	}
 
-	public String toString() {
-		String str = "[Group info: Name/number of the group - " + groupName + "; Specialization - " + specializ
-				+ "; Number of students in the group - " + numberOfStudents + "; ID - " + id + "]" + "\n";
-		if (curator != null) {
-			str += "\n Group " + groupName + " curator: " + curator.toString() + "\n";
-		}
-		int counter = 1;
-		for (int i = 0; i < students.length; i++) {
-			if (students[i] != null) {
-				str += "\t" + counter + ") " + students[i].toString() + "\n";
-				counter++;
-			}
-		}
-		return str;
-	}
+//	public String toString() {
+//		String str = "[Group info: Name/number of the group - " + groupName + "; Specialization - " + specializ
+//				+ "; Number of students in the group - " + numberOfStudents + "; ID - " + id + "]" + "\n";
+//		if (curator != null) {
+//			str += "\n Group " + groupName + " curator: " + curator.toString() + "\n";
+//		}
+//		int counter = 1;
+//		for (int i = 0; i < students.length; i++) {
+//			if (students[i] != null) {
+//				str += "\t" + counter + ") " + students[i].toString() + "\n";
+//				counter++;
+//			}
+//		}
+//		return str;
+//	}
 
 	public boolean addStudents(Student Stud) {
-		if (numberOfStudents < students.length) {
-			for (int i = 0; i < students.length; i++) {
-				if (students[i] == null) {
-					students[i] = Stud;
-					numberOfStudents++;
-					return true;
-				}
-			}
-		}
-		return false;
+		return students.add(Stud);
+//		if (numberOfStudents < students.length) {
+//			for (int i = 0; i < students.length; i++) {
+//				if (students[i] == null) {
+//					students[i] = Stud;
+//					numberOfStudents++;
+//					return true;
+//				}
+//			}
+//		}
+//		return false;
 	}
 
 	public boolean removeStudents(long id) {
