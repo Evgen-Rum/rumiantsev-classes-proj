1a4fa953faa678c4ad1ef4e472129a52600363e9
Evgenii Rumiantsev <new.twunk@mail.ru>
2021-09-16 14:37:02 +0300

    Create interface WorkWithDyn Arrays
    Added in interface six methods: size, add, remove, contains, get,
    toArray

---
+++ lesson4/src/datastructures/WorkWithDynArrays.java
@@ -0,0 +1,17 @@
+package datastructures;
+
+public interface WorkWithDynArrays {
+
+	int size();
+
+	void add(Object obj);
+
+	boolean remove(Object obj);
+
+	boolean contains(Object obj);
+
+	Object get();
+
+	Object[] toArray();
+
+}
