26c8ecec480107146a4c3f36a57d525fa615daf8
Evgenii Rumiantsev <new.twunk@mail.ru>
2021-09-21 14:05:12 +0300

    fixed method "toString"

--- lesson4/src/classes/Teacher.java
+++ lesson4/src/classes/Teacher.java
@@ -4,7 +4,8 @@ public class Teacher extends Employee implements Identifiable {
 
 	public String toString() {
 		return "[Employee info:  ID - " + getId() + "; Academic degree - " + getAcademDegree() + "; Position - "
-				+ getPosition() + "; Salary - " + getSalary() + " BYN" + "; Experience - " + getExperience() + "]";
+				+ getPosition() + "; Salary - " + getSalary() + " BYN" + "; Experience - " + getExperience()
+				+ "; Prize - " + getPrize() + "]";
 	}
 
 	@Override
