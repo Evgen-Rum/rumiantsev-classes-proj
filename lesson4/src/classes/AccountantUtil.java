aed45ce09b8e9aea48524343ba8d1eb3e66386d3
Evgenii Rumiantsev <new.twunk@mail.ru>
2021-09-21 14:08:47 +0300

    added some comments, remade method "addPrize"

--- lesson4/src/classes/AccountantUtil.java
+++ lesson4/src/classes/AccountantUtil.java
@@ -18,16 +18,16 @@ public class AccountantUtil extends Employee {
 		return false;
 	}
 
-	public static boolean addSalaryC(Cleaner clean) {
-		if (clean.getPosition().equals("Cleaner") && clean.getPosition() != null) {
-			if (clean.getExperience() > 0 && clean.getExperience() < 7) {
-				clean.setSalary(BigDecimal.valueOf(200).add(clean.getPrize()));
+	public static boolean addSalary(Employee empl) {
+		if (empl.getPosition().equals("Cleaner") && empl.getPosition() != null) {
+			if (empl.getExperience() > 0 && empl.getExperience() < 7) {
+				empl.setSalary(BigDecimal.valueOf(200).add(empl.getPrize()));
 				return true;
-			} else if (clean.getExperience() > 7 && clean.getExperience() < 15) {
-				clean.setSalary(BigDecimal.valueOf(800).add(clean.getPrize()));
+			} else if (empl.getExperience() > 7 && empl.getExperience() < 15) {
+				empl.setSalary(BigDecimal.valueOf(800).add(empl.getPrize()));
 				return true;
-			} else if (clean.getExperience() > 15) {
-				clean.setSalary(BigDecimal.valueOf(1100).add(clean.getPrize()));
+			} else if (empl.getExperience() > 15) {
+				empl.setSalary(BigDecimal.valueOf(1100).add(empl.getPrize()));
 				return true;
 			}
 		}
@@ -50,29 +50,56 @@ public class AccountantUtil extends Employee {
 		return false;
 	}
 
-	public static boolean addPrizeT(Teacher teach) {
-		if (teach.getExperience() > 0 && teach.getExperience() < 15) {
-			teach.setPrize(teach.getSalary().divide(teach.getSalary(), 3));
-			return true;
-		} else if (teach.getExperience() > 15) {
-			teach.setPrize(teach.getSalary().divide(teach.getSalary(), 2));
-			return true;
-		} else {
-			return false;
+//	public static boolean addPrizeT(Teacher teach) {
+//		if (teach.getExperience() > 0 && teach.getExperience() < 15) {
+//			teach.setPrize(teach.getSalary().divide(teach.getSalary(), 3));
+//			return true;
+//		} else if (teach.getExperience() > 15) {
+//			teach.setPrize(teach.getSalary().divide(teach.getSalary(), 2));
+//			return true;
+//		} else {
+//			return false;
+//		}
+//	}
+
+	public static boolean addPrize(Employee empl) {
+		if (empl.getPosition().equals("Teacher")) {
+			if (empl.getExperience() >= 5 && empl.getExperience() <= 10) {
+				empl.setPrize(BigDecimal.valueOf(100));
+				return true;
+			} else if (empl.getExperience() > 10 && empl.getExperience() < 15) {
+				empl.setPrize(BigDecimal.valueOf(200));
+				return true;
+			} else if (empl.getExperience() >= 15) {
+				empl.setPrize(BigDecimal.valueOf(300));
+				return true;
+			}
+		} else if (empl.getPosition().equals("Cleaner")) {
+			if (empl.getExperience() >= 5 && empl.getExperience() <= 10) {
+				empl.setPrize(BigDecimal.valueOf(50));
+				return true;
+			} else if (empl.getExperience() > 10 && empl.getExperience() < 15) {
+				empl.setPrize(BigDecimal.valueOf(100));
+				return true;
+			} else if (empl.getExperience() >= 15) {
+				empl.setPrize(BigDecimal.valueOf(150));
+				return true;
+			}
 		}
+		return false;
 	}
 
-	public static boolean addPrizeC(Cleaner clean) {
-		if (clean.getExperience() > 0 && clean.getExperience() < 15) {
-			clean.setPrize(clean.getSalary().divide(clean.getSalary(), 3));
-			return true;
-		} else if (clean.getExperience() > 15) {
-			clean.setPrize(clean.getSalary().divide(clean.getSalary(), 2));
-			return true;
-		} else {
-			return false;
-		}
-	}
+//	public static boolean addPrizeC(Cleaner clean) {
+//		if (clean.getExperience() > 0 && clean.getExperience() < 15) {
+//			clean.setPrize(clean.getSalary().divide(clean.getSalary(), 3));
+//			return true;
+//		} else if (clean.getExperience() > 15) {
+//			clean.setPrize(clean.getSalary().divide(clean.getSalary(), 2));
+//			return true;
+//		} else {
+//			return false;
+//		}
+//	}
 
 	public static boolean addHolidayPayT(Teacher teach) {
 		teach.setHolidayPay(teach.getSalary().multiply(teach.getSalary().divide(teach.getSalary(), 2)));
